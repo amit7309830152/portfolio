08be5c270d0549ed3c435cdc9bf75ad03558c95a
amit8896406322 <amit8896406322@gmail.com>
2020-07-13 22:58:44 +0530

    update mobile view with constant view

--- portfolio/public/javascripts/index.js
+++ portfolio/public/javascripts/index.js
@@ -1,38 +1,48 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        let scrl_top = $(window).scrollTop();
-        if (scrl_top < 10 && scrl_top < 100) {
-            $('.text-on-intro').removeClass('animate__animated animate__backOutRight');
-            $('.text-on-intro').addClass('animate__animated animate__bounceInLeft');
-        }
-        if (scrl_top > 10) {
-            $('.text-on-intro').removeClass('animate__animated animate__bounceInLeft');
-            $('.text-on-intro').addClass('animate__animated animate__backOutRight');
-        }
+    if ($(window).width() > 1000) {
+        $(window).scroll(function() {
+            let scrl_top = $(window).scrollTop();
+            if (scrl_top < 10 && scrl_top < 100) {
+                $('.text-on-intro').removeClass('animate__animated animate__backOutRight');
+                $('.text-on-intro').addClass('animate__animated animate__bounceInLeft');
+            }
+            if (scrl_top > 10) {
+                $('.text-on-intro').removeClass('animate__animated animate__bounceInLeft');
+                $('.text-on-intro').addClass('animate__animated animate__backOutRight');
+            }
 
+            if (scrl_top > 700) {
+                $('.tech-use-frontend').fadeIn(1000);
+                $('.reactjs').addClass('animate__animated animate__backInLeft');
+                $('.html').addClass('animate__animated animate__backInLeft');
+                $('.js').addClass('animate__animated animate__backInDown');
+                $('.css').addClass('animate__animated animate__backInRight');
+                $('.bootstrap').addClass('animate__animated animate__backInRight');
+            }
 
-        if (scrl_top > 700) {
-            $('.tech-use-frontend').fadeIn(1000);
-            $('.reactjs').addClass('animate__animated animate__backInLeft');
-            $('.html').addClass('animate__animated animate__backInLeft');
-            $('.js').addClass('animate__animated animate__backInDown');
-            $('.css').addClass('animate__animated animate__backInRight');
-            $('.bootstrap').addClass('animate__animated animate__backInRight');
-        }
+            if (scrl_top > 999) {
+                $('.tech-use-backend').fadeIn(1000);
+            }
 
-        if (scrl_top > 999) {
-            $('.tech-use-backend').fadeIn(1000);
-        }
+            if (scrl_top > 1600) {
+                $('.know-1').addClass('animate__animated animate__headShake');
+                $('.know-2').addClass('animate__animated animate__headShake');
+            }
 
-        if (scrl_top > 1600) {
-            $('.know-1').addClass('animate__animated animate__headShake');
-            $('.know-2').addClass('animate__animated animate__headShake');
-        }
-
-        if (scrl_top > 2138) {
-            $('.sayhello h1').addClass('animate__animated animate__swing');
-        }
-    });
+            if (scrl_top > 2138) {
+                $('.sayhello h1').addClass('animate__animated animate__swing');
+            }
+        });
+    } else {
+        $('.text-on-intro').show();
+        $('.tech-use-frontend').show();
+        $('.reactjs').show();
+        $('.html').show();
+        $('.js').show();
+        $('.css').show();
+        $('.bootstrap').show();
+        $('.tech-use-backend').show();
+    }
     setTimeout(function() {
         $('.text-on-intro').show()
         $('.text-on-intro').addClass('animate__animated animate__bounceInLeft');
